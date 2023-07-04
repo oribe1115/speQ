38c236689f2a170554dccb7f14ad4d48bf7c7d47
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-05 00:10:50 +0900

    ナビゲーションの表示の仕方を画面サイズによって変動するようにした

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -6,12 +6,19 @@ import { ref } from 'vue'
 import { UserInfo } from '@/apis/generated'
 
 const me = ref<UserInfo>()
+const drawer = ref(false)
+const pcDrawer = ref(true)
+
 apiClient.user.getMe().then((res) => (me.value = res))
 </script>
 
 <template>
-  <v-app-bar>
+  <v-app-bar color="primary">
+    <!-- スマホ画面でのみメニューアイコンを表示 -->
+    <v-app-bar-nav-icon @click="drawer = !drawer" class="d-flex d-lg-none"></v-app-bar-nav-icon>
+
     <v-app-bar-title>speQ</v-app-bar-title>
+
     <template v-slot:append>
       <div v-if="me !== undefined">
         <UserIcon :trap-id="me.traPId" />
@@ -19,4 +26,18 @@ apiClient.user.getMe().then((res) => (me.value = res))
       </div>
     </template>
   </v-app-bar>
+
+  <!-- PC画面用 -->
+  <v-navigation-drawer class="d-none d-sm-flex" v-model="pcDrawer">
+    <v-list>
+      <v-list-item> test </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <!-- スマホ画面用 -->
+  <v-navigation-drawer class="d-flex d-lg-none" v-model="drawer" temporary>
+    <v-list>
+      <v-list-item> test </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
