673e0ecf15a051e00402b1ccad56229b452b6461
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-04 16:20:56 +0900

    @/apisがFailed to resolve importとなる

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import apiClient from '@/apis'
+import { ContestInfo } from '@/apis/generated'
+
+const contestInfo = ref<ContestInfo>()
+
+apiClient.contest.getContestInfo().then((res) => (contestInfo.value = res))
+</script>
 
 <template>
-  <h1>Home</h1>
+  <h1>{{ contestInfo?.title }}</h1>
+  <p>{{ contestInfo?.description }}</p>
 </template>
