dcb03efadbb8fddbf0fb68032c25807592ff2147
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-04 15:58:50 +0900

    @でsrc以下にアクセスできるようにした

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,5 +1,5 @@
 import { RouteRecordRaw, createWebHistory, createRouter } from 'vue-router'
-import HomePage from './views/HomePage.vue'
+import HomePage from '@/views/HomePage.vue'
 
 export enum RouteName {
   Home = 'home'
