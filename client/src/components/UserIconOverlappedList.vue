2ad87f35355e2019236d204eb60a48075cab733f
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 12:35:29 +0900

    管理者情報を表示するようにした

---
+++ client/src/components/UserIconOverlappedList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import UserIcon from './UserIcon.vue'
+
+const props = defineProps<{
+  trapIds: string[]
+}>()
+</script>
+
+<template>
+  <div class="overlapped-list">
+    <UserIcon
+      v-for="(traPId, i) in props.trapIds"
+      :key="i"
+      :trap-id="traPId"
+      class="overlapped-item"
+    />
+  </div>
+</template>
+
+<style>
+.overlapped-item {
+  margin-left: -15px;
+}
+
+.overlapped-list:first-child {
+  margin-left: 0px;
+}
+</style>
--- client/src/views/AdminHomePage.vue
+++ client/src/views/AdminHomePage.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import apiClient from '@/apis'
+import { traPId } from '@/apis/generated'
+import UserIconOverlappedList from '@/components/UserIconOverlappedList.vue'
+
+const rootUsers = ref<traPId[]>([])
+const adminUsers = ref<traPId[]>([])
+
+apiClient.user.getRootUsers().then((res) => (rootUsers.value = [res]))
+apiClient.user.getAdmins().then((res) => (adminUsers.value = res))
+</script>
 
 <template>
-  <p>admin page</p>
+  <v-card>
+    <v-card-title> 管理者 </v-card-title>
+
+    <v-cart-text>
+      Root:
+      <UserIconOverlappedList :trap-ids="rootUsers" />
+    </v-cart-text>
+
+    <v-cart-text>
+      Admin:
+      <UserIconOverlappedList :trap-ids="adminUsers" />
+    </v-cart-text>
+  </v-card>
 </template>
