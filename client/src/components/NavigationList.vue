86cadf65c113930b0b7637452ebeda62da6482ee
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 11:23:46 +0900

    Navigationのlistを切り分けた

---
+++ client/src/components/NavigationList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { RouteName } from '@/router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <v-list>
+    <v-list-item nav @click="router.push(RouteName.Vote)"> 投票する </v-list-item>
+  </v-list>
+</template>
