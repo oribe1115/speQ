b664acf49acd927fe4d4927a7b0032293dc7de1f
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 18:35:45 +0900

    入力されたtraP IDに対してのバリデーションが表示されるようになった

--- client/src/components/TrapIdInputForm.vue
+++ client/src/components/TrapIdInputForm.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import UserIcon from './UserIcon.vue'
 import { ref } from 'vue'
+
+import UserIcon from '@/components/UserIcon.vue'
+import { useExistTrapIds } from '@/store/existTrapIds'
 
 const props = defineProps<{
   trapId: string
@@ -9,16 +11,30 @@ const props = defineProps<{
 const currentInput = ref<string>(props.trapId)
 
 defineEmits(['update:trapId', 'delete'])
+
+const { isExist } = useExistTrapIds()
+const rules = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  required: (value: any) => {
+    if (value === '') {
+      return 'Field is required'
+    } else if (!isExist(value)) {
+      return '存在しないtraP IDです'
+    }
+    return true
+  }
+}
 </script>
 
 <template>
-  <div class="d-flex">
-    <UserIcon :trap-id="currentInput" />
+  <div class="d-flex align-center" style="column-gap: 16px">
+    <UserIcon :trap-id="currentInput" class="mb-4" />
     <v-text-field
       prefix="@"
       v-model="currentInput"
       @update:model-value="$emit('update:trapId', currentInput)"
+      :rules="[rules.required]"
     ></v-text-field>
-    <v-btn icon="mdi-close" color="red" class="ml-4" @click="$emit('delete')"></v-btn>
+    <v-btn icon="mdi-close" color="red" class="mb-4" @click="$emit('delete')"></v-btn>
   </div>
 </template>
