26184b90ca1f60ee56b129bc68c9719a0fbdadd7
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 21:13:54 +0900

    存在するtraP IDで構成されているかをバリデーションするようにした

--- client/src/components/TrapIdListInputForm.vue
+++ client/src/components/TrapIdListInputForm.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 import { traPId } from '@/apis/generated'
 import TrapIdInputForm from '@/components/TrapIdInputForm.vue'
+import { useExistTrapIds } from '@/store/existTrapIds'
 
 const props = defineProps<{
   initialList: traPId[]
@@ -11,11 +12,17 @@ const props = defineProps<{
 
 const trapIds = ref<traPId[]>([...props.initialList])
 
+const { isExist } = useExistTrapIds()
+
 const removeTrapId = (index: number) => {
   trapIds.value.splice(index, 1)
 }
 const appendEmptyTrapId = () => {
   trapIds.value.push('')
+}
+
+const submittable = () => {
+  return trapIds.value.every((id) => isExist(id))
 }
 
 const clickOnSave = () => {
@@ -32,6 +39,12 @@ const clickOnSave = () => {
     </v-list>
     <v-btn icon="mdi-plus" class="align-self-center" :onclick="appendEmptyTrapId"></v-btn>
 
-    <v-btn @click="clickOnSave">Save</v-btn>
+    <v-btn
+      @click="clickOnSave"
+      :disabled="!submittable()"
+      :width="40"
+      class="align-self-center mt-10"
+      >Save</v-btn
+    >
   </div>
 </template>
