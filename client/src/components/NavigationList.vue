43714d2ce00326fbfd145dfd0cce09ecf1c6b464
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 11:39:02 +0900

    NavigationにAdmin用の領域を追加

--- client/src/components/NavigationList.vue
+++ client/src/components/NavigationList.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 
 import { RouteName } from '@/router'
+import { useMeStore } from '@/store/me'
 
 const router = useRouter()
+
+const { isAdminOrRoot } = storeToRefs(useMeStore())
 </script>
 
 <template>
   <v-list>
     <v-list-item nav @click="router.push(RouteName.Vote)"> 投票する </v-list-item>
+
+    <v-list-group value="Admin" v-if="isAdminOrRoot">
+      <template v-slot:activator="{ props }">
+        <v-list-item v-bind="props" title="Admin"></v-list-item>
+      </template>
+      <v-list-item>test</v-list-item>
+    </v-list-group>
   </v-list>
 </template>
