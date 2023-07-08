2c7f873679901d6e77963cebc5579ced54630e83
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-09 00:23:32 +0900

    日時系をテキストで入力するようにした

--- client/src/views/ContestInfoManagePage.vue
+++ client/src/views/ContestInfoManagePage.vue
@@ -5,6 +5,7 @@ import apiClient from '@/apis'
 import { ContestInfo } from '@/apis/generated'
 
 const contestInfo = ref<ContestInfo>({})
+const sampleDateText = '2023-04-01T14:15:22+09:00'
 
 apiClient.contest.getContestInfo().then((res) => (contestInfo.value = res))
 </script>
@@ -22,6 +23,57 @@ apiClient.contest.getContestInfo().then((res) => (contestInfo.value = res))
       <v-card-title>イベント概要</v-card-title>
       <v-card-text>
         <v-textarea v-model="contestInfo.description"></v-textarea>
+      </v-card-text>
+    </v-card-item>
+
+    <v-card-item>
+      <v-card-title>開始予定時刻</v-card-title>
+      <v-card-subtitle>競技開始前にトップページで表示</v-card-subtitle>
+
+      <v-card-text>
+        この時間にはシステム上ではイベントは開始されません。
+
+        <v-text-field
+          v-model="contestInfo.scheduled_start_time"
+          :placeholder="sampleDateText"
+        ></v-text-field>
+      </v-card-text>
+    </v-card-item>
+
+    <v-card-item>
+      <v-card-title>競技開始時刻</v-card-title>
+      <v-card-subtitle>システム上で競技が開始される時刻</v-card-subtitle>
+
+      <v-card-text>
+        競技中は様々な情報の変更が行えなくなります。
+        競技の開始準備が整ってから入力することを推奨します。
+
+        <v-text-field v-model="contestInfo.start_time" :placeholder="sampleDateText"></v-text-field>
+      </v-card-text>
+    </v-card-item>
+
+    <v-card-item>
+      <v-card-title>競技終了時刻</v-card-title>
+      <v-card-subtitle>システム上で競技が終了される時刻</v-card-subtitle>
+
+      <v-card-text>
+        競技の開始準備が整ってから入力することを推奨します。 競技中も編集が可能です。
+
+        <v-text-field v-model="contestInfo.end_time" :placeholder="sampleDateText"></v-text-field>
+      </v-card-text>
+    </v-card-item>
+
+    <v-card-item>
+      <v-card-title>投票フリーズ時刻</v-card-title>
+      <v-card-subtitle>投票が行えなくなる時刻</v-card-subtitle>
+
+      <v-card-text>
+        競技中も編集が可能です。
+
+        <v-text-field
+          v-model="contestInfo.voting_freeze_time"
+          :placeholder="sampleDateText"
+        ></v-text-field>
       </v-card-text>
     </v-card-item>
   </v-card>
