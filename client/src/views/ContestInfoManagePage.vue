f4ed09b0fe562dbfbf0d3d912036aa13ab4b21e4
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 23:12:53 +0900

    イベント名とイベント概要は入力できるようになった

--- client/src/views/ContestInfoManagePage.vue
+++ client/src/views/ContestInfoManagePage.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+import apiClient from '@/apis'
 import { ContestInfo } from '@/apis/generated'
 
 const contestInfo = ref<ContestInfo>({})
+
+apiClient.contest.getContestInfo().then((res) => (contestInfo.value = res))
 </script>
 
 <template>
   <v-card>
     <v-card-item>
       <v-card-title>イベント名</v-card-title>
+      <v-card-text>
+        <v-text-field v-model="contestInfo.title"></v-text-field>
+      </v-card-text>
+    </v-card-item>
 
-      <v-text-field v-model="contestInfo.title"></v-text-field>
+    <v-card-item>
+      <v-card-title>イベント概要</v-card-title>
+      <v-card-text>
+        <v-textarea v-model="contestInfo.description"></v-textarea>
+      </v-card-text>
     </v-card-item>
   </v-card>
 </template>
