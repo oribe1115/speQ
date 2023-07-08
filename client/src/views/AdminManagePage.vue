675a37053f5a3204e6225604c144785b5522729d
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-08 16:43:07 +0900

    AdminManagePageに載せる情報の概形

--- client/src/views/AdminManagePage.vue
+++ client/src/views/AdminManagePage.vue
@@ -1,3 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<template>adminユーザー管理ページ</template>
+import apiClient from '@/apis'
+import { traPId } from '@/apis/generated'
+
+const rootUsers = ref<traPId[]>([])
+
+apiClient.user.getRootUsers().then((res) => (rootUsers.value = [res]))
+</script>
+
+<template>
+  <v-card>
+    <v-card-title> Root </v-card-title>
+
+    <v-card-subtitle>rootユーザーはアプリケーション実行時の環境変数で設定されます</v-card-subtitle>
+    <v-card-text> </v-card-text>
+  </v-card>
+
+  <v-card class="mt-8">
+    <v-card-title> Admin </v-card-title>
+
+    <v-card-subtitle>イベントに関する設定や情報更新ができます</v-card-subtitle>
+    <v-card-text> </v-card-text>
+  </v-card>
+</template>
