d881b50797ea93c3eaeafcca7ed2e47820790d0e
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-09 16:53:07 +0900

    Prometheus用のAPIを追加

--- server/main.go
+++ server/main.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	e := echo.New()
+	e.GET("/api/metrics", echo.WrapHandler(promhttp.Handler()))
 	e.GET("/api/test", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, c.Request().Header)
 	})
