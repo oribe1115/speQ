652ba6a3406d8aac988192da0dcb55c09592725a
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-10 10:52:34 +0900

    sql.Null*とポインタの変換を整理した

--- server/router/contest.go
+++ server/router/contest.go
@@ -2,13 +2,13 @@ package router
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/exp/slog"
 	"net/http"
 	"speQ/generated/api"
 	"speQ/generated/model"
+	"speQ/util"
 )
 
 func (r *Router) PutContestInfo(c echo.Context) error {
@@ -48,64 +48,23 @@ func (r *Router) GetContestInfo(c echo.Context) error {
 }
 
 func convertContestInfo2api(modelValue *model.ContestInfo) api.ContestInfo {
-	apiValue := api.ContestInfo{}
-	apiValue.Title = &modelValue.Title
-
-	if modelValue.Description.Valid {
-		apiValue.Description = &modelValue.Description.String
+	return api.ContestInfo{
+		Title:              &modelValue.Title,
+		Description:        util.NullString2Pointer(modelValue.Description),
+		ScheduledStartTime: util.NullTime2Pointer(modelValue.ScheduledStartTime),
+		StartTime:          util.NullTime2Pointer(modelValue.StartTime),
+		EndTime:            util.NullTime2Pointer(modelValue.EndTime),
+		VotingFreezeTime:   util.NullTime2Pointer(modelValue.VotingFreezeTime),
 	}
-	if modelValue.ScheduledStartTime.Valid {
-		apiValue.ScheduledStartTime = &modelValue.ScheduledStartTime.Time
-	}
-	if modelValue.StartTime.Valid {
-		apiValue.StartTime = &modelValue.StartTime.Time
-	}
-	if modelValue.EndTime.Valid {
-		apiValue.EndTime = &modelValue.EndTime.Time
-	}
-	if modelValue.VotingFreezeTime.Valid {
-		apiValue.VotingFreezeTime = &modelValue.VotingFreezeTime.Time
-	}
-
-	return apiValue
 }
 
 func convertContestInfo2Model(apiValue *api.ContestInfo) *model.ContestInfo {
-	modelValue := &model.ContestInfo{}
-
-	if apiValue.Title != nil {
-		modelValue.Title = *apiValue.Title
+	return &model.ContestInfo{
+		Title:              util.Pointer2NullString(apiValue.Title).String,
+		Description:        util.Pointer2NullString(apiValue.Description),
+		ScheduledStartTime: util.Pointer2NullTime(apiValue.ScheduledStartTime),
+		StartTime:          util.Pointer2NullTime(apiValue.StartTime),
+		EndTime:            util.Pointer2NullTime(apiValue.EndTime),
+		VotingFreezeTime:   util.Pointer2NullTime(apiValue.VotingFreezeTime),
 	}
-	if apiValue.Description != nil {
-		modelValue.Description = sql.NullString{
-			String: *apiValue.Description,
-			Valid:  true,
-		}
-	}
-	if apiValue.ScheduledStartTime != nil {
-		modelValue.ScheduledStartTime = sql.NullTime{
-			Time:  *apiValue.ScheduledStartTime,
-			Valid: true,
-		}
-	}
-	if apiValue.StartTime != nil {
-		modelValue.StartTime = sql.NullTime{
-			Time:  *apiValue.StartTime,
-			Valid: true,
-		}
-	}
-	if apiValue.EndTime != nil {
-		modelValue.EndTime = sql.NullTime{
-			Time:  *apiValue.EndTime,
-			Valid: true,
-		}
-	}
-	if apiValue.VotingFreezeTime != nil {
-		modelValue.VotingFreezeTime = sql.NullTime{
-			Time:  *apiValue.VotingFreezeTime,
-			Valid: true,
-		}
-	}
-
-	return modelValue
 }
