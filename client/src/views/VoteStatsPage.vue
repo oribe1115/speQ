4594420de4176dbc58f585d237ee824f1a61aecd
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-07-11 14:54:52 +0900

    一位予想を表示した

--- client/src/views/VoteStatsPage.vue
+++ client/src/views/VoteStatsPage.vue
@@ -1,3 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<template>vote stats</template>
+import apiClient from '@/apis'
+import { VoteStatsItem } from '@/apis/generated'
+import UserIcon from '@/components/UserIcon.vue'
+import UserIconOverlappedList from '@/components/UserIconOverlappedList.vue'
+
+const voteStats = ref<VoteStatsItem[]>()
+
+apiClient.vote.getVoteStats().then((res) => (voteStats.value = res))
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>みんなの1位予想</v-card-title>
+
+    <v-card-text class="mt-4">
+      <v-lazy :model-value="voteStats !== undefined" v-if="voteStats">
+        <v-table>
+          <tbody>
+            <tr v-for="item in voteStats" :key="item.contestant" style="height: 70px">
+              <td class="w-25">
+                <UserIcon :trap-id="item.contestant" class="mr-4" />{{ item.contestant }}
+              </td>
+              <td><UserIconOverlappedList :trap-ids="item.voters" /></td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-lazy>
+    </v-card-text>
+  </v-card>
+</template>
